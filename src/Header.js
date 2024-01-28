4cca7534b951b9df38ee5cb9c13cabb345a26ab6
Chris How <chow0014@student.monash.edu>
2024-01-28 22:56:32 +1100

    Where this this?

---
+++ src/Header.js
@@ -0,0 +1,13 @@
+// Header.js
+import React from 'react';
+
+function Header() {
+  return (
+    <header>
+      <h1>My GAY App</h1>
+      <p>Gay ass black guys</p>
+    </header>
+  );
+}
+
+export default Header;
---
+++ src/MenuBar.js
@@ -0,0 +1,17 @@
+// MenuBar.js
+import React from 'react';
+
+function MenuBar() {
+  return (
+    <div className="menu-bar">
+      <ul>
+        <li><a href="#">Home</a></li>
+        <li><a href="#">About</a></li>
+        <li><a href="#">Services</a></li>
+        <li><a href="#">Contact</a></li>
+      </ul>
+    </div>
+  );
+}
+
+export default MenuBar;
